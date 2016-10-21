c918a6514da39fa61aac28213093662e8e1b6559
Versus <bladewolk@gmail.com>
2016-10-21 17:19:19 +0300

    fix verstka centr prices

--- resources/views/prices/create.blade.php
+++ resources/views/prices/create.blade.php
@@ -2,19 +2,18 @@
 
 @section('content')
     <div class="container">
-        <div class="form-group">
+        <div class="container col-md-3 col-md-offset-5">
             {{ Form::open(['route' => 'prices.store']) }}
             @foreach($devices as $index => $device)
                 {{ Form::radio('device_id', $device->id, !$index) }}
                 {{ Form::label($device->name) }}<br>
             @endforeach
             {{ Form::label('minTime') }}
-            {{ Form::number('minTime','', ['class' =>'form-control']) }}
+            {{ Form::number('minTime','', ['class' =>'form-control', 'autocomplete' => 'off']) }}
             {{ Form::label('Price for 1 minute') }}
-            {{ Form::text('value','', ['class' =>'form-control']) }}
+            {{ Form::text('value','', ['class' =>'form-control', 'autocomplete' => 'off']) }}
             {{ Form::submit('Create', ['class' => 'btn btn-primary']) }}
-
-            {{ Form::close() }}
+            {{ Form::close() }} <br>
         </div>
     </div>
     @if (count($errors) > 0)
--- resources/views/prices/edit.blade.php
+++ resources/views/prices/edit.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container col-md-3 col-md-offset-5">
         <div class="form-group">
             {{ Form::model($price, ['method'=>'PATCH', 'route' =>['prices.update', $price->id]]) }}
             {{ Form::label('minTime') }}
-            {{ Form::number('minTime',$price->minTime, ['class' =>'form-control']) }}
+            {{ Form::number('minTime',$price->minTime, ['class' =>'form-control', 'autocomplete' => 'off']) }}
             {{ Form::label('Price for 1 minute') }}
-            {{ Form::text('value',$price->value, ['class' =>'form-control']) }}
+            {{ Form::text('value',$price->value, ['class' =>'form-control', 'autocomplete' => 'off']) }}
             {{ Form::submit('Update', ['class' => 'btn btn-primary']) }}
             {{ Form::close() }}
         </div>
